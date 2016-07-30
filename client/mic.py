374d8877476865cc8c5adb87c77fe3b9e1152d3c
Laury Lafage <lowrey97x@hotmail.fr>
2016-07-30 10:06:15 +0200

    add listen from speech_recognition project.
    listen method should work faster and better.

--- client/mic.py
+++ client/mic.py
@@ -7,9 +7,9 @@ import tempfile
 import wave
 import audioop
 import pyaudio
-import alteration
-import jasperpath
-
+import math
+import collections
+from audiodata from AudioData
 
 class Mic:
 
@@ -72,6 +72,80 @@ class Mic:
             damping = self.dynamic_energy_adjustment_damping ** seconds_per_buffer # account for different chunk sizes and rates
             target_energy = energy * self.dynamic_energy_ratio
             self.energy_threshold = self.energy_threshold * damping + target_energy * (1 - damping)
+
+    def listen(self, timeout = None):
+        """
+        Records a single phrase from ``source`` (an ``AudioSource`` instance) into an ``AudioData`` instance, which it returns.
+        This is done by waiting until the audio has an energy above ``recognizer_instance.energy_threshold`` (the user has started speaking), and then recording until it encounters ``recognizer_instance.pause_threshold`` seconds of non-speaking or there is no more audio input. The ending silence is not included.
+        The ``timeout`` parameter is the maximum number of seconds that it will wait for a phrase to start before giving up and throwing an ``speech_recognition.WaitTimeoutError`` exception. If ``timeout`` is ``None``, it will wait indefinitely.
+        """
+
+        stream = self._audio.open(format=pyaudio.paInt16,
+                                  channels=1,
+                                  rate=self.RATE,
+                                  input=True,
+                                  frames_per_buffer=self.CHUNK)
+
+        seconds_per_buffer = (self.CHUNK + 0.0) / self.RATE
+        pause_buffer_count = int(math.ceil(self.pause_threshold / seconds_per_buffer)) # number of buffers of non-speaking audio before the phrase is complete
+        phrase_buffer_count = int(math.ceil(self.phrase_threshold / seconds_per_buffer)) # minimum number of buffers of speaking audio before we consider the speaking audio a phrase
+        non_speaking_buffer_count = int(math.ceil(self.non_speaking_duration / seconds_per_buffer)) # maximum number of buffers of non-speaking audio to retain before and after
+
+        # read audio input for phrases until there is a phrase that is long enough
+        elapsed_time = 0 # number of seconds of audio read
+        while True:
+            frames = collections.deque()
+
+            # store audio input until the phrase starts
+            while True:
+                elapsed_time += seconds_per_buffer
+                if timeout and elapsed_time > timeout: # handle timeout if specified
+                    raise WaitTimeoutError("listening timed out")
+
+                buffer = stream.read(self.CHUNK)
+                if len(buffer) == 0: break # reached end of the stream
+                frames.append(buffer)
+                if len(frames) > non_speaking_buffer_count: # ensure we only keep the needed amount of non-speaking buffers
+                    frames.popleft()
+
+                # detect whether speaking has started on audio input
+                energy = audioop.rms(buffer, self.SAMPLE_WIDTH) # energy of the audio signal
+                if energy > self.energy_threshold: break
+
+                # dynamically adjust the energy threshold using assymmetric weighted average
+                if self.dynamic_energy_threshold:
+                    damping = self.dynamic_energy_adjustment_damping ** seconds_per_buffer # account for different chunk sizes and rates
+                    target_energy = energy * self.dynamic_energy_ratio
+                    self.energy_threshold = self.energy_threshold * damping + target_energy * (1 - damping)
+
+            # read audio input until the phrase ends
+            pause_count, phrase_count = 0, 0
+            while True:
+                elapsed_time += seconds_per_buffer
+
+                buffer = stream.read(self.CHUNK)
+                if len(buffer) == 0: break # reached end of the stream
+                frames.append(buffer)
+                phrase_count += 1
+
+                # check if speaking has stopped for longer than the pause threshold on the audio input
+                energy = audioop.rms(buffer, self.SAMPLE_WIDTH) # energy of the audio signal
+                if energy > self.energy_threshold:
+                    pause_count = 0
+                else:
+                    pause_count += 1
+                if pause_count > pause_buffer_count: # end of the phrase
+                    break
+
+            # check how long the detected phrase is, and retry listening if the phrase is too short
+            phrase_count -= pause_count
+            if phrase_count >= phrase_buffer_count: break # phrase is long enough, stop listening
+
+        # obtain frame data
+        for i in range(pause_count - non_speaking_buffer_count): frames.pop() # remove extra non-speaking frames at the end
+        frame_data = b"".join(list(frames))
+
+        return AudioData(frame_data, source.RATE, source.SAMPLE_WIDTH)
 
     def __del__(self):
         self._audio.terminate()
